692e5f43caf2029ec548918a362bd91883ffb790
qwerty3690217804 <qwerty3690217804@gmail.com>
2023-08-17 02:17:50 -0700

    L Park

--- src/sudoku.js
+++ src/sudoku.js
@@ -34,13 +34,18 @@ const Sudoku = () => {
   const generatePuzzle = () => {
     const PUZZLE_SIZE = 9;
 
-    const newPuzzle = Array.from({ length: PUZZLE_SIZE }, () =>
-      Array.from({ length: PUZZLE_SIZE }, () => '')
+    const seedPuzzle = Array.from({ length: PUZZLE_SIZE }, () =>
+    Array.from({ length: PUZZLE_SIZE }, () => '')
     );
-  
-    // Generate a complete puzzle (e.g., by solving an empty puzzle)
+
+  fillRandom(seedPuzzle);
+
+  const newPuzzle = Array.from({ length: PUZZLE_SIZE }, (_, rowIndex) =>
+    [...seedPuzzle[rowIndex]]
+  );
+
     const solvedPuzzle = solve(newPuzzle);
-  
+
     let minClues = 0;
     let maxClues = 0;
   
@@ -54,7 +59,27 @@ const Sudoku = () => {
       minClues = 19;
       maxClues = 26;
     }
-    // Remove cells to create the initial puzzle
+    
+    const filledIndexes = Array.from(
+      { length: PUZZLE_SIZE * PUZZLE_SIZE },
+      (_, index) => index
+    );
+  
+    for (let i = filledIndexes.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [filledIndexes[i], filledIndexes[j]] = [
+        filledIndexes[j],
+        filledIndexes[i],
+      ];
+    }
+  
+    for (const index of filledIndexes) {
+      const row = Math.floor(index / PUZZLE_SIZE);
+      const col = index % PUZZLE_SIZE;
+  
+      newPuzzle[row][col] = solvedPuzzle[row][col];
+    }
+
     const cellsToRemove = Math.floor(Math.random() * (maxClues - minClues + 1) + minClues); 
     let removedCount = 0;
   
@@ -66,17 +91,73 @@ const Sudoku = () => {
         const backup = solvedPuzzle[row][col];
         solvedPuzzle[row][col] = '';
         
-        // Check if the puzzle still has a unique solution
         const copy = JSON.parse(JSON.stringify(solvedPuzzle));
         if (solve(copy)) {
           removedCount++;
         } else {
-          solvedPuzzle[row][col] = backup; // Restore the removed value
+          solvedPuzzle[row][col] = backup; 
         }
       }
     }
   
     setPuzzle(solvedPuzzle);
+  };
+
+  const fillRandom = (puzzle) => {
+    const PUZZLE_SIZE = puzzle.length;
+    const digits = Array.from({ length: PUZZLE_SIZE }, (_, index) => (index + 1).toString());
+  
+    const fillCell = (row, col) => {
+      if (row === PUZZLE_SIZE) {
+        return true;
+      }
+      
+      const nextRow = col === PUZZLE_SIZE - 1 ? row + 1 : row;
+      const nextCol = (col + 1) % PUZZLE_SIZE;
+  
+      shuffleArray(digits);
+  
+      for (const digit of digits) {
+        if (isValid(puzzle, row, col, digit)) {
+          puzzle[row][col] = digit;
+          if (fillCell(nextRow, nextCol)) {
+            return true;
+          }
+          puzzle[row][col] = '';
+        }
+      }
+      return false;
+    };
+  
+    fillCell(0, 0);
+  };
+  
+  const isValid = (puzzle, row, col, digit) => {
+    for (let i = 0; i < puzzle.length; i++) {
+      if (puzzle[row][i] === digit || puzzle[i][col] === digit) {
+        return false;
+      }
+    }
+  
+    const startRow = Math.floor(row / 3) * 3;
+    const startCol = Math.floor(col / 3) * 3;
+    
+    for (let i = startRow; i < startRow + 3; i++) {
+      for (let j = startCol; j < startCol + 3; j++) {
+        if (puzzle[i][j] === digit) {
+          return false;
+        }
+      }
+    }
+    
+    return true;
+  };
+  
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
   };
   
   return (
